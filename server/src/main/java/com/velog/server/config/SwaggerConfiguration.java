81b70a0052f49f3745fa116c9f6762cba8697ba0
HyeBin <skdisk7368@naver.com>
2021-10-10 05:46:35 +0900

    [Add] swagger config 추가

---
+++ server/src/main/java/com/velog/server/config/SwaggerConfiguration.java
@@ -0,0 +1,35 @@
+package com.velog.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(swaggerInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("com.velog.server.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false); // 기본으로 세팅되는 200, 401, 403, 404 메시지를 표시하지 않음
+    }
+
+    private ApiInfo swaggerInfo() {
+        return new ApiInfoBuilder()
+                .title("Velog API Documentation")
+                .description("Velog 서버 API에 대한 연동 문서입니다.")
+                .license("YangHyeBin")
+                .licenseUrl("")
+                .version("1")
+                .build();
+    }
+}
